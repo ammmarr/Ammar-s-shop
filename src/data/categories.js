ccaf72eb1bcec3c273786336f3a318ec5d717bfc
ammmarr <ammarrelkosii@yahoo.com>
2022-12-30 02:09:20 +0200

    x

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,15 +1,20 @@
+import categImg1 from "../assets/Chairs.jpg"
+import categImg2 from "../assets/Lamps.jpg"
+import categImg3 from "../assets/Handmade-pots.jpg"
+
+
 const categories = [{
-    img: "../src/assets/6844f5d4a050c2425450f0ab251a43b8.JPG",
+    img: categImg1,
     desc:"Costum Wood Work",
     title:"Top Picks",
     fontColor: "#39312d"
   },{
-    img: "../src/assets/170e111c1c4218734b194cc7561c72c9.JPG",
+    img: categImg2,
     desc:"Recycled Metal",
     title:"Special offer",
     fontColor: "#0f4a63",
   },{
-    img: "../src/assets/Handmade-pots.jpg",
+    img: categImg3,
     desc:"Handmade Pottery",
     title:"Minimal decore",
     fontColor: "#442a1c"
